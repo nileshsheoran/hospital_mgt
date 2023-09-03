31d62d31f058108682a763b9a50cccde5a194ff3
Nitin <nsheoran2005@gmail.com>
2023-09-03 13:45:33 +0530

    main screen commit removed

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo',debugShowCheckedModeBanner: false,
       routes: routeMap,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
