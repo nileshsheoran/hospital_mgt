7c181d336b4f231730b1b4cda02afbd1a0544bc1
Nitin <nsheoran2005@gmail.com>
2023-09-03 10:10:19 +0530

    show patient screen added

---
+++ lib/patient/ui/show_patient_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowPatientScreen extends StatefulWidget {
+  const ShowPatientScreen({super.key});
+
+  @override
+  State<ShowPatientScreen> createState() => _ShowPatientScreenState();
+}
+
+class _ShowPatientScreenState extends State<ShowPatientScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
