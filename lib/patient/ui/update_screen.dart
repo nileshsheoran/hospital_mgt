91fda9fffb4c5545bee3231e2e42854b55e52cb6
Nitin <nsheoran2005@gmail.com>
2023-09-03 11:45:41 +0530

    update screen added

---
+++ lib/patient/ui/update_screen.dart
@@ -0,0 +1,147 @@
+
+import 'package:flutter/material.dart';
+import 'package:hospital_mgt/database/database_helper.dart';
+import 'package:hospital_mgt/patient/model/patient_model.dart';
+
+class UpdateScreen extends StatefulWidget {
+  const UpdateScreen({required this.patient, super.key});
+
+  final Patient patient;
+
+  @override
+  State<UpdateScreen> createState() => _UpdateScreenState();
+}
+
+class _UpdateScreenState extends State<UpdateScreen> {
+  late TextEditingController idController;
+  late TextEditingController nameController;
+  late TextEditingController diseaseController;
+  late TextEditingController feesController;
+  late TextEditingController doctorController;
+  late TextEditingController addressController;
+
+  @override
+  void initState() {
+    Patient patient = widget.patient;
+
+    idController =
+        TextEditingController(text: patient.id.toString());
+    nameController = TextEditingController(text: patient.name);
+    diseaseController= TextEditingController(text: patient.disease);
+    feesController= TextEditingController(text: patient.fees.toString());
+    doctorController= TextEditingController(text: patient.doctor);
+    addressController = TextEditingController(text: patient.address);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update Patient'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: idController,
+                keyboardType: TextInputType.number,
+                enabled: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: ''
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: '',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: diseaseController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: '',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: feesController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: '',
+                ),
+              ),
+              TextField(
+                controller: doctorController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: '',
+                ),
+              ),
+              TextField(
+                controller: addressController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: '',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    Patient patient = Patient(
+                      id: int.parse(idController.text),
+                      name: nameController.text,
+                      disease: diseaseController.text,
+                      fees: int.parse(feesController.text),
+                      doctor: doctorController.text,
+                      address: addressController.text,
+                    );
+                    await DatabaseHelper.updatePatient(patient);
+                    if(mounted) {
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text(
+                    'Update',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
