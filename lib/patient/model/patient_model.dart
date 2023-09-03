63caaa3040ccd0f4b3b69daba71382d132c54ad9
Nitin <nsheoran2005@gmail.com>
2023-09-03 11:47:09 +0530

    patient model screen create

--- lib/patient/model/patient_model.dart
+++ lib/patient/model/patient_model.dart
@@ -1,1 +1,39 @@
-class Patient {}
+class Patient {
+  int id;
+  String name;
+  String disease;
+  int fees;
+  String doctor;
+  String address;
+
+  Patient({
+    required this.id,
+    required this.name,
+    required this.disease,
+    required this.fees,
+    required this.doctor,
+    required this.address,
+  });
+
+  static Patient fromMap(Map<String, dynamic> map) {
+    return Patient(
+      id: map['Id'],
+      name: map['Name'],
+      disease: map['Disease'],
+      fees: map['Fees'],
+      doctor: map['Doctor'],
+      address: map['Address'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'Id': id,
+      'Name': name,
+      'Disease': disease,
+      'Fees': fees,
+      'Doctor': doctor,
+      'Address': address,
+    };
+  }
+}
