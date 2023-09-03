fe11a2ee1770109718c84cca2c607170d342937d
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-09-03 15:30:44 +0530

    create categoryModel class and category databaseHelper create addCategory create showCategory showDoctorScreen implement

--- lib/doctor/ui/show_doctor_screen.dart
+++ lib/doctor/ui/show_doctor_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_mgt/database/database_helper.dart';
+
+import '../model/doctor_model.dart';
 
 class ShowDoctorScreen extends StatefulWidget {
   const ShowDoctorScreen({super.key});
@@ -8,8 +11,67 @@ class ShowDoctorScreen extends StatefulWidget {
 }
 
 class _ShowDoctorScreenState extends State<ShowDoctorScreen> {
+  List<Doctor>doctorList=[];
+  @override
+  @override
+  void initState() {
+    showDoctorData();
+    super.initState();
+  }
+
+  Future showDoctorData() async {
+    doctorList = await DatabaseHelper.getDoctorData();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(child: Scaffold(
+      appBar: AppBar(title: const Text(''),),
+      body: ListView.builder(
+          itemCount: doctorList.length,
+          itemBuilder:(context,index){
+        Doctor doctor=doctorList[index];
+        return Column(
+          children: [
+            Row(
+              children: [
+                const Text('id'),
+                const SizedBox(width: 100,),
+                Text(doctor.id.toString()),
+              ],
+            ),
+            Row(
+              children: [
+                const Text('Name'),
+                const SizedBox(width: 100,),
+                Text(doctor.name),
+              ],
+            ),
+            Row(
+              children: [
+                const Text('Specialist'),
+                const SizedBox(width: 100,),
+                Text(doctor.specialist),
+              ],
+            ),
+            Row(
+              children: [
+                const Text('Fees'),
+                const SizedBox(width: 100,),
+                Text(doctor.fees.toString()),
+              ],
+            ),
+            Row(
+              children: [
+                const Text('Timing'),
+                const SizedBox(width: 100,),
+                Text(doctor.timing.toString()),
+              ],
+            ),
+          ],
+        );
+      }),
+    ));
   }
 }
